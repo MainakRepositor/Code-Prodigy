f03249fcb93b0910fab1a9ba86a27437318e944b
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:34:51 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,15 @@
+# Number of sub-arrays fitting a criteria
+
+from collections import defaultdict
+
+def fn(arr, k):
+    counts = defaultdict(int)
+    counts[0] = 1
+    ans = curr = 0
+
+    for num in arr:
+        # do logic to change curr
+        ans += counts[curr - k]
+        counts[curr] += 1
+    
+    return ans
