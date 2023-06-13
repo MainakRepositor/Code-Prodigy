4bf91cabbcba5c85066b46571dc2491e167e316a
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:35:16 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,14 @@
+# Monotonic increasing stack
+
+def fn(arr):
+    stack = []
+    ans = 0
+
+    for num in arr:
+        # for monotonic decreasing, just flip the > to <
+        while stack and stack[-1] > num:
+            # do logic
+            stack.pop()
+        stack.append(num)
+    
+    return ans
