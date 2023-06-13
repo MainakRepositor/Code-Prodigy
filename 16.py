370128521f74965ab4a412db87e150fc68598f68
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:39:12 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,12 @@
+# Find top K elements
+import heapq
+
+def fn(arr, k):
+    heap = []
+    for num in arr:
+        # do some logic to push onto heap according to problem's criteria
+        heapq.heappush(heap, (CRITERIA, num))
+        if len(heap) > k:
+            heapq.heappop(heap)
+    
+    return [num for num in heap]
