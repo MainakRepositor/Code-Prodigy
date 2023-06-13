c331faa9bc641eedb71c793c7aab85a595627d67
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:36:29 +0530

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,15 @@
+# Binary Tree DFS : iterative
+
+def dfs(root):
+    stack = [root]
+    ans = 0
+
+    while stack:
+        node = stack.pop()
+        # do logic
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+
+    return ans
