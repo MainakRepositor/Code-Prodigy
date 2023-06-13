0812d32cdda4abadc7ba51b79799594eabdabbe1
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:35:57 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,12 @@
+# Binary Tree Recursive : DFA (recursive)
+
+def dfs(root):
+    if not root:
+        return
+    
+    ans = 0
+
+    # do logic
+    dfs(root.left)
+    dfs(root.right)
+    return ans
