905aca0f585965ad75dc3cfbbba5787369568bae
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:33:32 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,13 @@
+# Fast and slow pointer in Linked list
+
+def fn(head):
+    slow = head
+    fast = head
+    ans = 0
+
+    while fast and fast.next:
+        # do logic
+        slow = slow.next
+        fast = fast.next.next
+    
+    return ans
