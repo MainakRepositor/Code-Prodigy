02e2fc79b0cbc394da18c090ce20d3bfd962b8a9
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-06-13 18:38:01 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,15 @@
+# Graph DFS : iterative
+def fn(graph):
+    stack = [START_NODE]
+    seen = {START_NODE}
+    ans = 0
+
+    while stack:
+        node = stack.pop()
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                stack.append(neighbor)
+    
+    return ans
